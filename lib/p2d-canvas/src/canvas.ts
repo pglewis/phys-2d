b33a1084c9db0aacee2a033d901740ff3c9d544f
pglewis <pglewis13@gmail.com>
2025-04-07 18:57:00 -0400

    Canvas.drawRect()

--- lib/p2d-canvas/src/canvas.ts
+++ lib/p2d-canvas/src/canvas.ts
@@ -7,8 +7,17 @@ export type CanvasProps = {
 export type CircleProps = {
 	position: {x: number, y: number}
 	radius: number
-	filled?: boolean
 	color?: string | CanvasGradient | CanvasPattern
+	filled?: boolean
+};
+
+export type RectProps = {
+	x: number
+	y: number
+	width: number
+	height: number
+	color?: string | CanvasGradient | CanvasPattern
+	filled?: boolean
 };
 
 export class Canvas {
@@ -59,71 +68,25 @@ export class Canvas {
 		}
 	}
 
-	drawGround() {
-		const {ctx, width} = this;
-
-		ctx.beginPath();
-		ctx.strokeStyle = 'rgb(240, 188, 128)';
-		ctx.lineWidth = 0.4;
-		ctx.moveTo(0, -0.2);
-		ctx.lineTo(width, -0.2);
-		ctx.stroke();
-	}
-
-	drawBob() {
+	drawRect(props: RectProps) {
 		const {ctx} = this;
-		const x = 2.8;
-		const y = 1.98; // Bob is 1.98M tall
+		const {
+			x, y,
+			width, height,
+			color = '#000',
+			filled = true
+		} = props;
 
 		ctx.beginPath();
+		ctx.rect(x, y, width, height);
+		ctx.closePath();
 
-		ctx.strokeStyle = '#4444cc';
-		ctx.lineWidth = .23;
-
-		ctx.moveTo(x, 0);
-		ctx.lineTo(x, y);
-		ctx.stroke();
-	}
-
-	drawBasket() {
-		const {ctx} = this;
-		const bottomOfBackboard = 2.84;
-		const hBackboard = 1.07;
-		const heightOfRim = 3.048;
-		const backOfRim = 1.2192;
-		const rimToBackboard = .151;
-		const rimDiameter = .4572;
-		const netLength = .43;
-
-		ctx.beginPath();
-
-		ctx.lineWidth = .03;
-		ctx.strokeStyle = '#882222';
-
-		// Draw the backboard
-		ctx.moveTo(backOfRim, bottomOfBackboard);
-		ctx.lineTo(backOfRim, bottomOfBackboard + hBackboard);
-		ctx.stroke();
-
-		// Draw the rim
-		ctx.moveTo(backOfRim, heightOfRim);
-		ctx.lineTo(backOfRim + rimDiameter + rimToBackboard, heightOfRim);
-		ctx.stroke();
-
-		// Draw the net
-		ctx.moveTo(backOfRim + rimToBackboard + 0.03, heightOfRim);
-		ctx.lineTo(backOfRim + rimToBackboard + 0.1 + 0.03, heightOfRim - netLength);
-		ctx.stroke();
-		ctx.moveTo(backOfRim + rimToBackboard + rimDiameter - 0.03, heightOfRim);
-		ctx.lineTo(backOfRim + rimToBackboard + rimDiameter - 0.1 - 0.03, heightOfRim - netLength);
-		ctx.stroke();
-
-		// Draw the pole
-		ctx.beginPath();
-		ctx.lineWidth = .07;
-		ctx.moveTo(backOfRim, heightOfRim + .25);
-		ctx.lineTo(.04, 2.6);
-		ctx.lineTo(.04, 0);
-		ctx.stroke();
+		if (filled) {
+			ctx.fillStyle = color;
+			ctx.fill();
+		} else {
+			ctx.strokeStyle = color;
+			ctx.stroke();
+		}
 	}
 }
