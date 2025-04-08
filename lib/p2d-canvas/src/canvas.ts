dbc56a8fe6c453631d1692786b258c01fb9531ea
pglewis <pglewis13@gmail.com>
2025-04-07 23:02:41 -0400

    Canvas.drawLine() and a wrapper for the native canvas addEventListener

--- lib/p2d-canvas/src/canvas.ts
+++ lib/p2d-canvas/src/canvas.ts
@@ -4,11 +4,11 @@ export type CanvasProps = {
 	height?: number
 };
 
-export type CircleProps = {
-	position: {x: number, y: number}
-	radius: number
+export type LineProps = {
+	p1: {x: number, y: number}
+	p2: {x: number, y: number}
+	width: number
 	color?: string | CanvasGradient | CanvasPattern
-	filled?: boolean
 };
 
 export type RectProps = {
@@ -16,6 +16,13 @@ export type RectProps = {
 	y: number
 	width: number
 	height: number
+	color?: string | CanvasGradient | CanvasPattern
+	filled?: boolean
+};
+
+export type CircleProps = {
+	position: {x: number, y: number}
+	radius: number
 	color?: string | CanvasGradient | CanvasPattern
 	filled?: boolean
 };
@@ -42,30 +49,34 @@ export class Canvas {
 		this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
 	}
 
+	addEventListener<K extends keyof HTMLElementEventMap>(
+		type: K,
+		listener: (ev: HTMLElementEventMap[K]) => void,
+		options?: boolean | AddEventListenerOptions
+	): void {
+		this.canvas.addEventListener(type, listener, options);
+	}
+
 	clear() {
 		this.ctx.clearRect(0, 0, this.width, this.height);
 	}
 
-	drawCircle(props: CircleProps) {
+	drawLine(props: LineProps) {
 		const {ctx} = this;
 		const {
-			position,
-			radius,
+			p1,
+			p2,
+			width,
 			color = '#000',
-			filled = true,
 		} = props;
 
+		ctx.strokeStyle = color;
+		ctx.lineWidth = width;
 		ctx.beginPath();
-		ctx.arc(position.x, position.y, radius, 0.0, 2.0 * Math.PI);
+		ctx.moveTo(p1.x, p1.y);
+		ctx.lineTo(p2.x, p2.y);
 		ctx.closePath();
-
-		if (filled) {
-			ctx.fillStyle = color;
-			ctx.fill();
-		} else {
-			ctx.strokeStyle = color;
-			ctx.stroke();
-		}
+		ctx.stroke();
 	}
 
 	drawRect(props: RectProps) {
@@ -79,6 +90,28 @@ export class Canvas {
 
 		ctx.beginPath();
 		ctx.rect(x, y, width, height);
+		ctx.closePath();
+
+		if (filled) {
+			ctx.fillStyle = color;
+			ctx.fill();
+		} else {
+			ctx.strokeStyle = color;
+			ctx.stroke();
+		}
+	}
+
+	drawCircle(props: CircleProps) {
+		const {ctx} = this;
+		const {
+			position,
+			radius,
+			color = '#000',
+			filled = true,
+		} = props;
+
+		ctx.beginPath();
+		ctx.arc(position.x, position.y, radius, 0.0, 2.0 * Math.PI);
 		ctx.closePath();
 
 		if (filled) {
